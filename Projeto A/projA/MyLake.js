6e167bee533c8d1b4ff870b7ed15e2010c056be3
João Martins <up201707311@fe.up.pt>
2019-04-05 00:10:12 +0100

    added circle and lake to the scene

---
+++ Projeto A/projA/MyLake.js
@@ -0,0 +1,35 @@
+/**
+* MyLake
+* @constructor
+*/
+class MyLake extends CGFobject {
+    constructor(scene, scaleFactor) {
+        super(scene);
+        this.scaleFactor = scaleFactor;
+        this.circle = new MyCircle(scene,20);
+
+        //water material
+        this.waterTex = new CGFappearance(this.scene);
+        this.waterTex.loadTexture("./images/Textures/water_texture.jpg");
+        this.waterTex.setDiffuse(0.8,0.8,0.8,1);
+        this.waterTex.setSpecular(1.0,1.0,1.0,1);
+        this.waterTex.setShininess(100);
+        this.initBuffers();
+    }
+    initBuffers() {
+        this.circle.initBuffers();
+    }
+    
+    display() {
+        this.waterTex.apply();
+        this.scene.pushMatrix();
+        this.scene.translate(1/8,0,1/8);
+        this.circle.display();
+        this.scene.popMatrix();
+        this.scene.pushMatrix();
+        this.scene.scale(2*this.scaleFactor,2*this.scaleFactor,2*this.scaleFactor);
+        this.scene.translate(-1,0,-1);
+        this.circle.display();
+        this.scene.popMatrix();
+    }
+}
