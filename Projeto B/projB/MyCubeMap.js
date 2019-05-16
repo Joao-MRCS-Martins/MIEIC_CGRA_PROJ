520062c10d5325da1016cb4efe4ce000908792bf
João Martins <up201707311@fe.up.pt>
2019-05-16 15:51:00 +0100

    added last files // keys interpreters

---
+++ Projeto B/projB/MyCubeMap.js
@@ -0,0 +1,59 @@
+class MyCubeMap extends CGFobject {
+    constructor(scene, scalefactor) {
+        super(scene);
+        this.scene = scene;
+        this.baseCube = new MyUnitCube(scene);
+        this.scalefactor = scalefactor;
+        this.dayToggle = true;
+        
+        this.dayTex = new CGFappearance(this.scene);
+        this.dayTex.setAmbient(1,1,1,1);
+        this.dayTex.setDiffuse(0, 0, 0, 0);
+        this.dayTex.setSpecular(0, 0, 0, 0);
+        this.dayTex.setShininess(10.0);
+        this.dayTex.loadTexture('images/Textures/Daylight.png');
+        this.dayTex.setTextureWrap('CLAMP_TO_EDGE', 'CLAMP_TO_EDGE');
+        
+        this.nightTex = new CGFappearance(this.scene);
+        this.nightTex.setAmbient(1,1,1,1);
+        this.nightTex.setDiffuse(0, 0, 0, 0);
+        this.nightTex.setSpecular(0, 0, 0, 0);
+        this.nightTex.setShininess(10.0);
+        this.nightTex.loadTexture('images/Textures/night.png');
+        this.nightTex.setTextureWrap('CLAMP_TO_EDGE', 'CLAMP_TO_EDGE');
+        
+
+    }
+
+    initBuffers() {
+        baseCube.initBuffers();
+    }
+
+    display() {
+        if(this.dayToggle){
+    this.dayTex.apply();
+    }
+    else{
+        this.nightTex.apply();
+    }
+    this.scene.pushMatrix();
+    this.scene.scale(this.scalefactor,this.scalefactor,this.scalefactor);
+    //this.scene.scale(-1,-1,-1);
+    this.baseCube.display();
+    
+
+    this.scene.popMatrix();
+
+    }
+
+    toggleDay(day){
+        this.dayToggle= day;
+    }
+
+    updateBuffers() {}
+
+
+    disableNormalViz() {
+
+    }
+}
